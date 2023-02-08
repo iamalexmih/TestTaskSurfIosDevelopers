b4cc32a172769fec7b4fc7dce1fa60193b5f4996
Alex <alexmihgel@gmail.com>
2023-02-08 23:22:30 +0300

    change on Delegate Data Source

--- testTaskSurfIosDevelopers/BottomSheetScreen/BottomSheetViewController.swift
+++ testTaskSurfIosDevelopers/BottomSheetScreen/BottomSheetViewController.swift
@@ -21,6 +21,7 @@ class BottomSheetViewController: UIViewController {
     var sections: [SectionModel] = []
     var createLayout: CreateLayout = CreateLayout()
     var dataSourceDiffable: UICollectionViewDiffableDataSource<SectionModel, DirectionItems>?
+    var numbItems = 10000
     private var collectionView: UICollectionView!
 
     
@@ -71,8 +72,7 @@ class BottomSheetViewController: UIViewController {
         sections = MockData.shared.getSections()
         
         setupCollectionView()
-        createDataSource()
-        reloadDataDiffableSnapshot()
+//        createDataSource()
         
         addSubviewOnView()
         setupConstraints()
@@ -119,13 +119,16 @@ private extension BottomSheetViewController {
 
 
 // MARK: config collection view
-private extension BottomSheetViewController {
+extension BottomSheetViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func setupCollectionView() {
         collectionView = UICollectionView(frame: .zero,
                                           collectionViewLayout: createLayout.createCompositionalLayout())
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = .white
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         collectionView.register(UICollectionViewCell.self,
                                 forCellWithReuseIdentifier: UICollectionViewCell.cellId)
@@ -137,54 +140,87 @@ private extension BottomSheetViewController {
         
     }
 
+
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        sections.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        sections[section].items.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        switch self.sections[indexPath.section].type {
+        case "TwoRows":
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DirecionCollectionViewCell.cellId,
+                                                          for: indexPath) as! DirecionCollectionViewCell
+            cell.setup(title: sections[indexPath.section].items[indexPath.row].direction)
+            cell.sizeToFit()
+            return cell
+        default:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DirecionCollectionViewCell.cellId,
+                                                          for: indexPath) as! DirecionCollectionViewCell
+            cell.setup(title: sections[indexPath.section].items[indexPath.row].direction)
+            cell.sizeToFit()
+            
+            return cell
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                         withReuseIdentifier: SectionHeaderForOneRow.reuseId,
+                                                                         for: indexPath) as! SectionHeaderForOneRow
+            sectionHeader.configHeader(textHeader: self.sections[indexPath.section].title)
+            return sectionHeader
+        default:
+            return UICollectionReusableView()
+        }
+    }
     
     // MARK: - Manage the data source
-    
-    func createDataSource() {
-        dataSourceDiffable = UICollectionViewDiffableDataSource<SectionModel, DirectionItems>(
-            collectionView: collectionView, cellProvider: { [weak self]
-                collectionView, indexPath, item in
-               
-                guard let self = self else { return nil }
-                switch self.sections[indexPath.section].type {
-                case "TwoRows":
-                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DirecionCollectionViewCell.cellId,
-                                                                  for: indexPath) as! DirecionCollectionViewCell
-                    cell.setup(title: item.direction)
-                    cell.sizeToFit()
-                    return cell
-                default:
-                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DirecionCollectionViewCell.cellId,
-                                                                  for: indexPath) as! DirecionCollectionViewCell
-                    cell.setup(title: item.direction)
-                    cell.sizeToFit()
-                    
-                    return cell
-                }
-        })
-        
-        dataSourceDiffable?.supplementaryViewProvider = { [weak self]
-            (collectionView, kind, indexPath) -> UICollectionReusableView? in
-            guard let sectionHeader = collectionView.dequeueReusableSupplementaryView(
-                ofKind: kind,
-                withReuseIdentifier: SectionHeaderForOneRow.reuseId,
-                for: indexPath) as? SectionHeaderForOneRow else { return nil }
-            
-            sectionHeader.configHeader(textHeader: self?.sections[indexPath.section].title)
-            return sectionHeader
-        }
-    }
-    
-    func reloadDataDiffableSnapshot() {
-        var snapshot = NSDiffableDataSourceSnapshot<SectionModel, DirectionItems>()
-        snapshot.appendSections(sections)
-        
-        for section in sections {
-            snapshot.appendItems(section.items, toSection: section)
-        }
-        
-        dataSourceDiffable?.apply(snapshot)
-    }
+//
+//    func createDataSource() {
+//        dataSourceDiffable = UICollectionViewDiffableDataSource<SectionModel, DirectionItems>(
+//            collectionView: collectionView, cellProvider: { [weak self]
+//                collectionView, indexPath, item in
+//
+//                guard let self = self else { return nil }
+//                switch self.sections[indexPath.section].type {
+//                case "TwoRows":
+//                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DirecionCollectionViewCell.cellId,
+//                                                                  for: indexPath) as! DirecionCollectionViewCell
+//                    cell.setup(title: item.direction)
+//                    cell.sizeToFit()
+//                    return cell
+//                default:
+//                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DirecionCollectionViewCell.cellId,
+//                                                                  for: indexPath) as! DirecionCollectionViewCell
+//                    cell.setup(title: item.direction)
+//                    cell.sizeToFit()
+//
+//                    return cell
+//                }
+//        })
+//
+//        dataSourceDiffable?.supplementaryViewProvider = { [weak self]
+//            (collectionView, kind, indexPath) -> UICollectionReusableView? in
+//            guard let sectionHeader = collectionView.dequeueReusableSupplementaryView(
+//                ofKind: kind,
+//                withReuseIdentifier: SectionHeaderForOneRow.reuseId,
+//                for: indexPath) as? SectionHeaderForOneRow else { return nil }
+//
+//            sectionHeader.configHeader(textHeader: self?.sections[indexPath.section].title)
+//            return sectionHeader
+//        }
+//    }
 }
 
 // MARK: config collection view
